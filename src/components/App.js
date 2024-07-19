8eb5a9a75b04c2177a9f4c8ff656dc508c351293
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-07-19 12:05:42 +0530

    changes commited

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@
 import React from "react";
 import './../styles/App.css';
 import { useState } from "react";
+
 const App = () => {
-  const [name,setName]=useState(" ");
+  const [name,setName]=useState('');
 
 const handleInputChange = (event) => {
   setName(event.target.value);
@@ -14,11 +15,12 @@ const handleInputChange = (event) => {
         <h1>Enter your name:</h1>
         <input type="text"
            value={name} 
-           onChange={handleInputChange} 
-        >
-        </input>
-        <p>{name}</p> 
-
+           onChange={handleInputChange } 
+           
+        />
+        
+       
+        {name && <p>Hello{name}!</p>}
     </div>
   )
 }
